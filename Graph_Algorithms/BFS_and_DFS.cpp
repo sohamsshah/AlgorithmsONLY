6bf781c0bd1c8b35669ae617eaa2fed47dce32b8
sohamsshah <sohamshah456@gmail.com>
2020-10-09 16:19:47 +0530

    add BFS and DFS

---
+++ Graph_Algorithms/BFS_and_DFS.cpp
@@ -0,0 +1,150 @@
+// BFS TRAVERSAL
+#include<iostream> 
+#include <list> 
+  
+using namespace std; 
+  
+// This class represents a directed graph using 
+// adjacency list representation 
+class Graph 
+{ 
+    int V;    // No. of vertices 
+  
+    // Pointer to an array containing adjacency lists 
+    list<int> *adj;    
+
+public: 
+    Graph(int V);  // Constructor 
+  
+    // function to add an edge to graph 
+    void addEdge(int v, int w);  
+  
+    // prints BFS traversal from a given source s 
+    void BFS(int s); 
+    void DFSUtil(int v, bool visited[]);
+    void DFS(int v);
+}; 
+  
+Graph::Graph(int V) 
+{ 
+    this->V = V; 
+    adj = new list<int>[V]; 
+} 
+  
+void Graph::addEdge(int v, int w) 
+{ 
+    adj[v].push_back(w); // Add w to v’s list. 
+}
+
+void Graph::DFSUtil(int v, bool visited[]) 
+{ 
+    // Mark the current node as visited and 
+    // print it 
+    visited[v] = true; 
+    cout << v << " "; 
+  
+    // Recur for all the vertices adjacent 
+    // to this vertex 
+    list<int>::iterator i; 
+    for (i = adj[v].begin(); i != adj[v].end(); ++i) 
+        if (!visited[*i]) 
+            DFSUtil(*i, visited); 
+} 
+
+void Graph::DFS(int v) 
+{ 
+    // Mark all the vertices as not visited 
+    bool *visited = new bool[V]; 
+    for (int i = 0; i < V; i++) 
+        visited[i] = false; 
+  
+    // Call the recursive helper function 
+    // to print DFS traversal 
+    DFSUtil(v, visited); 
+} 
+  
+void Graph::BFS(int s) 
+{ 
+    // Marking all the vertices as not-visited 
+    bool *visited = new bool[V]; 
+    for(int i = 0; i < V; i++) 
+        visited[i] = false; 
+  
+    // Create a queue for BFS 
+    list<int> queue; 
+  
+    // Mark the current node as visited and enqueue it 
+    visited[s] = true; 
+    queue.push_back(s); 
+  
+    list<int>::iterator i; 
+  
+    while(!queue.empty()) 
+    { 
+        // Dequeue  
+        s = queue.front(); 
+        cout << s << " "; 
+        queue.pop_front(); 
+  
+        // Get all adjacent vertices of the dequeued vertex s. If a adjacent has not been visited, visit it and mark it
+        for (i = adj[s].begin(); i != adj[s].end(); ++i) 
+        { 
+            if (!visited[*i]) 
+            { 
+                visited[*i] = true; 
+                queue.push_back(*i); 
+            } 
+        } 
+    } 
+} 
+
+
+  
+// Driver program to test methods of graph class 
+int main() 
+{ 
+    // Create a graph  
+    Graph g(4); // total vertices: 4
+    g.addEdge(0, 1); 
+    g.addEdge(0, 2); 
+    g.addEdge(1, 2); 
+    g.addEdge(2, 0); 
+    g.addEdge(2, 3); 
+    g.addEdge(3, 3);
+
+    cout << "Following is Breadth First Traversal "
+         << "(starting from vertex 0) \n"; 
+    g.BFS(0); 
+    
+    cout << "\nFollowing is Breadth First Traversal "
+         << "(starting from vertex 1) \n"; 
+    g.BFS(1); 
+    
+    cout << "\nFollowing is Breadth First Traversal "
+         << "(starting from vertex 2) \n"; 
+    g.BFS(2); 
+
+    cout << "\nFollowing is Breadth First Traversal "
+         << "(starting from vertex 3) \n"; 
+    g.BFS(3); 
+    
+    cout << "\nFollowing is Depth First Traversal "
+         << "(starting from vertex 0) \n"; 
+    g.DFS(0); 
+    
+    cout << "\nFollowing is Depth First Traversal "
+         << "(starting from vertex 1) \n"; 
+    g.DFS(1); 
+    
+    cout << "\nFollowing is Depth First Traversal "
+         << "(starting from vertex 2) \n"; 
+    g.DFS(2); 
+
+    cout << "\nFollowing is Depth First Traversal "
+         << "(starting from vertex 3) \n"; 
+    g.DFS(3); 
+    
+  
+    return 0; 
+} 
+
