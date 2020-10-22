8b84a1d15ef1a20bdf8135b0a13ba2644268d8f8
sohamsshah <sohamshah456@gmail.com>
2020-10-22 12:04:22 +0530

    add iterative DFS

--- Graph_Algorithms/Python/DFS.py
+++ Graph_Algorithms/Python/DFS.py
@@ -22,6 +22,21 @@ class Graph:
     def DFS(self,v):
         visited = [False]*(max(self.graph)+1)
         self.DFSUtil(v,visited)
+        self.DFS_iterative(v,visited)
+    def DFS_iterative(self,v, visited):
+        stack = [v]
+        while stack:
+            v = stack[-1]  
+            stack.pop() 
+            if (not visited[v]):  
+                print(v,end=' ')
+                visited[v] = True
+            for node in self.graph[v]:  
+                if (not visited[node]):  
+                    stack.append(node)  
+                    
+            
+        
 g = Graph() 
 g.addEdge(0, 1) 
 g.addEdge(0, 2) 
@@ -31,5 +46,6 @@ g.addEdge(2, 3)
 g.addEdge(3, 3) 
   
 print("Following is DFS from (starting from vertex 2)") 
-g.DFS(2) 
+g.DFS(2)
+g.DFS_iterative(2)
     
