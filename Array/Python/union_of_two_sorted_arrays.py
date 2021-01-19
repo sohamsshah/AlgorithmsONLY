05a872a5c0bf72c2b349aa2c1bca7078aed3b1ab
sohamsshah <sohamshah456@gmail.com>
2021-01-19 22:28:02 +0530

    add union of two sorted arrays program

---
+++ Array/Python/union_of_two_sorted_arrays.py
@@ -0,0 +1,26 @@
+def printUnion(arr1, arr2, m, n):
+    i, j = 0, 0
+    while i < m and j < n:
+        if arr1[i] < arr2[j]:
+            print(arr1[i])
+            i += 1
+        elif arr2[j] < arr1[i]:
+            print(arr2[j])
+            j+= 1
+        else:
+            print(arr2[j])
+            j += 1
+            i += 1
+    while i < m:
+        print(arr1[i])
+        i += 1
+
+    while j < n:
+        print(arr2[j])
+        j += 1
+
+arr1 = [1, 2, 4, 5, 6]
+arr2 = [2, 3, 5, 7]
+m = len(arr1)
+n = len(arr2)
+printUnion(arr1, arr2, m, n)
